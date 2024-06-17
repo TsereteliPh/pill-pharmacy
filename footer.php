46a37b9751df6eeabfb0008b46700fa113d74301
TsereteliPh <filipptsereteli@yandex.ru>
2024-06-17 15:23:26 +0300

    add and done .footer

--- footer.php
+++ footer.php
@@ -1,7 +1,17 @@
 </main>
 
 <footer class="footer">
+	<div class="container footer__container">
+		<a href="<?php echo bloginfo( 'url' ); ?>" class="footer__logo" aria-label="Логотип Аптечной сети Таблетка"></a>
+	</div>
 
+	<div class="footer__links">
+		<div class="container footer__links-container">
+			<a href="<?php echo get_privacy_policy_url(); ?>" class="footer__links-policy">Политика конфиденциальности</a>
+
+			<div class="footer__links-rights">Все права защищены <?php echo date( 'Y' ); ?></div>
+		</div>
+	</div>
 </footer>
 
 <?php get_template_part('layouts/partials/modals'); ?>
