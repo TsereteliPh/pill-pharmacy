d6163fe828822d61d2056e45761826f417caa601
TsereteliPh <filipptsereteli@yandex.ru>
2024-06-20 13:22:59 +0300

    add and done .why-us

---
+++ layouts/blocks/why-us/template.php
@@ -0,0 +1,27 @@
+<section class="why-us">
+	<div class="container">
+		<?php get_template_part( '/layouts/partials/title', null, array(
+			'class' => 'why-us__title',
+			'title' => get_sub_field( 'title' )
+		) ); ?>
+
+		<?php if ( get_sub_field( 'text' ) ) : ?>
+			<div class="why-us__text"><?php the_sub_field( 'text' ); ?></div>
+		<?php endif; ?>
+
+		<?php
+			$advantages = get_sub_field( 'advantages' );
+			if ( $advantages ) :
+				?>
+
+				<ul class="reset-list why-us__list">
+					<?php foreach ( $advantages as $advantage ) : ?>
+						<li class="why-us__item"><?php echo $advantage['text']; ?></li>
+					<?php endforeach; ?>
+				</ul>
+
+				<?php
+			endif;
+		?>
+	</div>
+</section>
