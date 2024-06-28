df7750d63387d1ec6713a4890437f3d29ac6f7e6
TsereteliPh <filipptsereteli@yandex.ru>
2024-06-28 13:28:02 +0300

    add and done .pills-text

---
+++ acfe-php/group_667e882be20ff.php
@@ -0,0 +1,60 @@
+<?php 
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_667e882be20ff',
+	'title' => 'Block:pills-text',
+	'fields' => array(
+		array(
+			'key' => 'field_667e883f00352',
+			'label' => 'Текст',
+			'name' => 'text',
+			'aria-label' => '',
+			'type' => 'textarea',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'acfe_textarea_code' => 0,
+			'maxlength' => '',
+			'rows' => 6,
+			'placeholder' => '',
+			'new_lines' => '',
+			'acfe_field_group_condition' => 0,
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => false,
+	'description' => '',
+	'show_in_rest' => 0,
+	'acfe_autosync' => array(
+		0 => 'php',
+	),
+	'acfe_form' => 0,
+	'acfe_display_title' => '',
+	'acfe_meta' => '',
+	'acfe_note' => '',
+	'modified' => 1719570189,
+));
+
+endif;
---
+++ layouts/blocks/pills-text/template.php
@@ -0,0 +1,5 @@
+<section class="pills-text">
+	<div class="container">
+		<div class="pills-text__text"><?php the_sub_field( 'text' ); ?></div>
+	</div>
+</section>
